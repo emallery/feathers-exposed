46367dd2fe95bac172c0ebc557211dd7f3fd15fa
Evan Mallery <emallery@vt.edu>
2023-07-27 00:47:23 -0400

    Added noot button

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <button @click="count++">Noot</button>
+  <p>Noot n{{ "o".repeat(count) }}t!</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const count = ref(2);
+    return {
+      count
+    }
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button {
+  box-shadow: 5px 5px 5px -4px rgba(0, 0, 0, 0.52);
+}
+</style>
